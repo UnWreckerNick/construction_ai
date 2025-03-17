44efcde39f8d7b3048590fec6e646f16950695ff
Nick <unwrecker2@gmail.com>
2025-03-17 16:19:25 +0700

    Move sensitive data to env

--- app/database.py
+++ app/database.py
@@ -1,11 +1,15 @@
+import os
+from dotenv import load_dotenv
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
 
 from app.models import Model
 
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_async_engine("sqlite+aiosqlite:///projects.db")
+engine = create_async_engine(DATABASE_URL)
 SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
--- app/services.py
+++ app/services.py
@@ -1,7 +1,9 @@
+import os
+from dotenv import load_dotenv
 import google.generativeai as genai
 
-
-GEMINI_API_KEY = "you_api_key"
+load_dotenv()
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel("gemini-2.0-flash")
